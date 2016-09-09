dfb0e115b0cf72eeb9d89304718db99ab93e2a84
ace <andrew.emil@gmail.com>
2016-09-08 23:14:41 -0400

    Music volume controller added

--- Assets/Cardboard/Scripts/MenuController.cs
+++ Assets/Cardboard/Scripts/MenuController.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MenuController : MonoBehaviour {
 
+	public Slider musicSlider;
 	// Use this for initialization
 	void Start () {
 	
@@ -27,5 +29,10 @@ public class MenuController : MonoBehaviour {
 
 	public void LoadLevelSelection() {
 		Application.LoadLevel ("LevelScene");
+	}
+
+	public void SetMusicVolume() {
+		AudioSource musicPlayer = GameObject.Find ("MusicPlayer").GetComponent<AudioSource> ();
+		musicPlayer.volume = musicSlider.value;
 	}
 }
