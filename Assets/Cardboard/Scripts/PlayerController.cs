111bb8632173f5717a03efc8dddf46eb059959ab
ace <andrew.emil@gmail.com>
2016-09-23 19:07:20 -0400

    Removed cubes

--- Assets/Cardboard/Scripts/PlayerController.cs
+++ Assets/Cardboard/Scripts/PlayerController.cs
@@ -33,7 +33,7 @@ public class PlayerController : MonoBehaviour {
 
 	public void Link(GameObject target) {
 		linkedTarget = target;
-		linkedTarget.GetComponent<CubeController> ().SetLinked (true);
+		linkedTarget.GetComponent<SphereController> ().SetLinked (true);
 		beam.maxParticles = 100;
 		isLinked = true;
 	}
@@ -47,7 +47,7 @@ public class PlayerController : MonoBehaviour {
 
 	public void Unlink() {
 		if (isLinked) {
-			linkedTarget.GetComponent<CubeController> ().SetLinked (false);
+			linkedTarget.GetComponent<SphereController> ().SetLinked (false);
 		}
 		isLinked = false;
 		linkedTarget = null;
@@ -80,8 +80,7 @@ public class PlayerController : MonoBehaviour {
 
 	//Used to clear links on new level
 	public void Clear() {
-		isLinked = false;
-		linkedTarget = null;
+		Unlink ();
 	}
 
 	public void Success() {
